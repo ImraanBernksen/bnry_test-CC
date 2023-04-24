3163452bf3e580bf890a04a42ff6d1d78aeee4fb
ImraanBernksen <imraanbernksen@gmail.com>
2023-04-24 11:26:59 +0200

    awe

---
+++ bnry_backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const axios = require('axios');
+
+const port = process.env.PORT || 3030;
+
+const newsArticles = '5f1441b653004bdb8851bb7444905c8b';
+
+app.get('/news', (req, res) => {
+  const url = `https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=${newsArticles}`;
+  axios.get(url)
+    .then(response => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('THIS IS A ERROR');
+    });
+});
+
+
+app.listen(port, () => {
+  console.log(`BACKEND PORT IS RUNNING!!! ${port}`);
+});
