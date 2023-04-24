bcded1e7bf56534c1f8fd1f96352c3b4c7b99d75
ImraanBernksen <imraanbernksen@gmail.com>
2023-04-24 11:21:38 +0200

    first commit

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="about">
+    <h2 style="color: aqua;">About</h2>
+<div class="container">
+    <p class="text-start" style="font-size: 27px;">Current Chronicle is a reputable online news platform that provides timely and accurate news coverage on various topics ranging from politics, business, technology, entertainment, sports, and more. With a team of experienced and dedicated journalists, the platform delivers insightful analysis and in-depth reporting on current events and breaking news stories from around the world.<br>
+    Whether you're looking for the latest political developments, updates on the stock market, reviews of new gadgets, or highlights of the latest sports games, Current Chronicle has got you covered. The platform is committed to delivering news stories that are impartial, objective, and informative, ensuring that readers are well-informed and up-to-date on the latest happenings.<br>
+    In addition to news coverage, Current Chronicle also offers opinion pieces, editorials, and feature articles that provide a deeper understanding of the events and issues shaping our world. The website is designed to be easy to navigate, with intuitive sections and search functions that allow readers to find the information they need quickly and easily.<br>
+    With a focus on accuracy, objectivity, and journalistic integrity, Current Chronicle is a trusted source of news and information for readers around the world.</p>
+</div>
+</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+/*   
+#about {
+    background: url(https://i.postimg.cc/XNkxpB71/manolo-chretien-Rp-C-9e0-ORNM-unsplash.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    width: 100%;
+    height: 100vh;
+    border-top: 2px solid #2D277D;
+    color: #2D277D;
+}
+*/
+</style>
---
+++ src/components/newsArticle.vue
@@ -0,0 +1,75 @@
+<template>
+<div id="newsArticle">
+    <h2 style="color: aqua;">All articles about Tesla from the last month, sorted by recent first</h2>
+    <div class="container" style="padding-bottom: 20px;">
+        <form class="d-flex" role="search" style="justify-content: center; padding: 30px;">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="width: 40%;">
+            <button class="btn btn-outline-light" type="submit">Search</button>
+        </form>
+    <div class="card mb-3 p-3" v-for="article in articles" :key="article" style="width: 100%; background-color: aqua; color: #2D277D;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img :src="article.urlToImage" class="img-fluid rounded-start mt-2" style="width: 30rem;">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body text-start">
+                <h3 class="card-title">{{ article.author }}</h3>
+                <h4 class="card-title">{{ article.title }}</h4>
+                <p class="card-text">{{ article.description }}</p>
+                <p class="card-text">{{ article.content }}</p>
+                <p class="card-title">{{ article.publishedAt }}</p>
+                <p class="card-text"><a :href="article.url">{{ article.url }}</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- news -->
+    <!-- <div class="container" style="padding-bottom: 40px;">
+    <div class="row" style="gap: 2rem; justify-content: center;">
+    <div class="card" v-for="article in articles" :key="article" style="width: 19rem; background-color: aqua;">
+        <h3 class="card-title">{{ article.author }}</h3>
+        <h4 class="card-title">{{ article.title }}</h4>
+        <p class="card-text">{{ article.description }}</p>
+        <a :href="article.url">More Information</a>
+        <img :src="article.urlToImage" class="card-img-top mt-2">
+        <h5 class="card-title">{{ article.publishedAt }}</h5>
+        <p class="card-text">{{ article.content }}</p>
+    </div>
+    </div>
+    </div> -->
+</div>
+</template>
+<script>
+import { useStore } from 'vuex';
+import { computed } from '@vue/runtime-core'
+export default {
+        setup() {
+        const store = useStore()
+        store.dispatch("fetchArticles")
+        const articles = computed(() => store.state.articles)
+        return {
+            articles
+        }
+      },
+}
+</script>
+<style scoped>
+.newsArticle {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+  }
+img {
+    border-radius: 10px;
+}
+.card { 
+    border-radius: 10px;
+    box-shadow: 2px 2px 2px 1px white;
+}
+.card:hover {
+    border-radius: 10px;
+    box-shadow: 2px 2px 2px 1px #2D277D;
+}
+</style>
